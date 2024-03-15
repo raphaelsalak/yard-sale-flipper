33b1e964b941580c2d01e897ae1c1e2818acda43
raphael <salak.raphael@heb.com>
2024-03-15 09:25:34 -0500

    added image qr code functionality

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,12 +2,21 @@
 import Image from "next/image";
 import { useState } from "react";
 import "./assets/styles.css" 
+import Html5QrcodePlugin from "./Html5QrcodePlugin"
 export default function Home() { 
   const [inputValue, setInputValue] = useState('')
   const [data, setData] = useState(null) 
+  const [decodedText, setDecodedText] = useState('')
+
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
-  }
+  }    
+
+  const onNewScanResult = (decodedText, decodedResult) => {
+    // handle decoded results here
+    setInputValue(decodedText)
+  };
+
   const handleButtonClick = () => {
     makeApiCall(inputValue)
 
@@ -21,24 +30,32 @@ export default function Home() {
     const fetchedData = await response.json()
     setData(fetchedData)
   }
+
   const sortedData = data?.itemSummaries.sort((a, b) => {
     const priceA = parseFloat(a.price.value);
     const priceB = parseFloat(b.price.value);
     return priceB - priceA;
   });
+
   return (
-    <>
+  <>
     <div className="center-container">
       <div className="card-container">
         <input
-        className="input-field"
-        type="text"
-        value={inputValue}
-        onChange={handleInputChange}
-        placeholder="enter product name"> 
+          className="input-field"
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="enter product name"> 
         </input>
         <button className="submit-button" onClick={handleButtonClick}>submit</button>
       </div>
+      <Html5QrcodePlugin
+        fps={10}
+        qrbox={250}
+        disableFlip={false}
+        qrCodeSuccessCallback={onNewScanResult}
+      />
       <div className="card-container">      
         {sortedData?.map(item => (
           <div key={item.itemId} className="card">
@@ -48,7 +65,7 @@ export default function Home() {
       ))}
       </div>
     </div>
-    </>
+  </>
    
   );
 }
