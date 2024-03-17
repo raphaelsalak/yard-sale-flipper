728a549a481be80cfa3ed67ee427582586c7dad2
raphael <salak.raphael@heb.com>
2024-03-17 08:52:06 -0500

    added offer input value and profit field functionality

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,11 +5,16 @@ import "./assets/styles.css"
 import Html5QrcodePlugin from "./Html5QrcodePlugin"
 export default function Home() { 
   const [inputValue, setInputValue] = useState('')
+  const [offerValue, setOfferValue] = useState('')
   const [data, setData] = useState(null) 
   const [decodedText, setDecodedText] = useState('')
+  const [profit, setProfit] = useState('')
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
+  }    
+  const handleOfferChange = (event) => {
+    setOfferValue(event.target.value);
   }    
 
   const onNewScanResult = (decodedText, decodedResult) => {
@@ -19,6 +24,16 @@ export default function Home() {
 
   const handleButtonClick = () => {
     makeApiCall(inputValue)
+    
+  }
+  const calculateProfit = () => {
+    let difference = 0
+    let formattedNumber = 0
+    if(sortedData != null){
+      difference = offerValue - sortedData[0].price.value
+      formattedNumber = difference.toFixed(2);
+    }
+    setProfit(formattedNumber)
 
   }
  
@@ -29,6 +44,7 @@ export default function Home() {
     })
     const fetchedData = await response.json()
     setData(fetchedData)
+    calculateProfit()
   }
 
   const sortedData = data?.itemSummaries.sort((a, b) => {
@@ -40,6 +56,13 @@ export default function Home() {
   return (
   <>
     <div className="center-container">
+      <input
+        className="input-field"
+        type="number"
+        value={offerValue}
+        onChange={handleOfferChange}
+        placeholder="enter offer"> 
+      </input>
       <div className="card-container">
         <input
           className="input-field"
@@ -64,6 +87,7 @@ export default function Home() {
           </div>
       ))}
       </div>
+      <p>Profit: {profit}</p>
     </div>
   </>
    
